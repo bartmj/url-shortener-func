d0bf7b02a894f9f38f5f606234e36e6cc6cc1328
bartmj <bartek.m.janusz@gmail.com>
2026-02-22 14:24:15 +0100

    second

--- function_app.py
+++ function_app.py
@@ -26,27 +26,6 @@ def ShortenUrl(req: func.HttpRequest) -> func.HttpResponse:
              status_code=200
         )
 
-@app.route(route="ShortenUrl", auth_level=func.AuthLevel.ANONYMOUS)
-def ShortenUrl(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
-
 @app.route(route="Redirect", auth_level=func.AuthLevel.ANONYMOUS)
 def Redirect(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
